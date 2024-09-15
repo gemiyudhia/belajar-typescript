fb383eb6ecab34d4d3e9ec210899c696125eecaa
Gemi <githuba122@gmail.com>
2024-09-16 00:41:39 +0700

    cara menggunakan props di typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 
-type User = {
-  username: string;
-  email: string;
-}
+type ButtonProps = {
+  className?: string;
+  onClick: () => void;
+  children: ReactNode;
+};
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className={`p-4 bg-blue-500 text-white rounded-md ${props.className}`}
+    >
+      {props.children}
+    </button>
+  );
+};
+
 function App() {
-const [myState, setMyState] = useState<number>(0)
-const [currentUser, setCurrentUser] = useState<User>()// dari objek User yang sudah di declare sebelumnya
-  
-const changeState = () => {
-  setMyState(myState + 1)
-  setCurrentUser({
-    username: "",
-    email: "",
-  })
-}
+  const [myState, setMyState] = useState<number>(0);
+
+  const changeState = () => {
+    setMyState(myState + 1);
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <button onClick={changeState} className="p-4 bg-blue-500 text-white rounded-md">
-        Click Me
-      </button>
-
-      <p>State: {myState}</p>
+      <Button onClick={() => {}}>Click Me</Button>
     </div>
   );
 }
