497ab74dd8791b80a0780aaa46cf373e96a9d731
Gemi <githuba122@gmail.com>
2024-09-16 00:52:50 +0700

    cara menggunakan input di typescript

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,26 @@ const Button = (props: ButtonProps) => {
 
 function App() {
   const [myState, setMyState] = useState<number>(0);
+  const [text, setText] = useState<string>("");
 
   const changeState = () => {
     setMyState(myState + 1);
   };
 
+  const handleText: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    // React.ChangeEventHandler<HTMLInputElement> berasal dari onChange
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen">
+      <input
+        onChange={handleText}
+        type="text"
+        className="p-4 border rounded-md bg-slate-600"
+      />
+
       <Button onClick={() => {}}>Click Me</Button>
     </div>
   );
