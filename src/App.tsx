c693cbb598154648c9487923046e5c631418e3bb
Gemi <githuba122@gmail.com>
2024-09-16 00:27:33 +0700

    cara membuat state menggunakan typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 
+type User = {
+  username: string;
+  email: string;
+}
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const [myState, setMyState] = useState<number>(0)
+const [currentUser, setCurrentUser] = useState<User>()// dari objek User yang sudah di declare sebelumnya
+  
+const changeState = () => {
+  setMyState(myState + 1)
+  setCurrentUser({
+    username: "",
+    email: "",
+  })
 }
 
-export default App
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <button onClick={changeState} className="p-4 bg-blue-500 text-white rounded-md">
+        Click Me
+      </button>
+
+      <p>State: {myState}</p>
+    </div>
+  );
+}
+
+export default App;
